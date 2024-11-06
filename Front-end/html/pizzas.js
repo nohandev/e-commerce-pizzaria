caef723a4b2eaea75499419a7b17284623d0ddb7
nohandev <nohanpablo759@gmail.com>
2024-11-06 14:09:20 -0300

    Add files via upload

---
+++ Front-end/html/pizzas.js
@@ -0,0 +1,115 @@
+let pizzaJson=[
+  {
+    id: 1,
+    name: 'Mussarela',
+    img: '../img-pizzas/pizza.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, camada dupla de mussarela e orégano'
+  },
+
+  {
+    id: 2,
+    name: 'Calabresa',
+    img: '../img-pizzas/pizza2.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+  },
+  {
+    id: 3,
+    name: 'Quatro Queijos',
+    img: '../img-pizzas/pizza3.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+  },
+  {
+    id: 4,
+    name: 'Brasileira',
+    img: '../img-pizzas/pizza4.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+  },
+  {
+    id: 5,
+    name: 'Portuguesa',
+    img: '../img-pizzas/pizza5.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+  },
+  {
+    id: 6,
+    name: 'Moda da Casa',
+    img: '../img-pizzas/pizza6.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+  },
+  {
+    id: 7,
+    name: 'Banana com canela',
+    img: '../img-pizzas/pizza7.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Mussarela, banana, canela e açúcar'
+  },
+  {
+    id: 8,
+    name: 'Chocolate com morango',
+    img: '../img-pizzas/pizza8.png',
+    price: [
+       20.00,
+       35.00, 
+       50.00
+    ],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Creme de leite, lascas de chocolate e morangos'
+  },
+  {
+    id: 9,
+    name: 'Bacon',
+    img: '../img-pizzas/pizza10.png',
+    price: [20.00, 35.00, 50.00],
+    sizes:[
+      "45cm",
+      "70cm",
+      "90cm"
+    ],
+    description: 'Molho de tomate, Camada dupla de mussarela, orégano e bacon'
+  }
+];
